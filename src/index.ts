ff0b4563ef1617f8c3a3c8ef6f6cca9ce54f0201
Bright Mabhiza <specifixperts@gmail.com>
2025-11-15 15:45:04 +0200

    first 1.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,44 @@
-import express from 'express'
+import express from "express"
+import createError from "http-errors"
 
 const app = express()
 
-app.get('/', (_req, res) => {
-  res.send('Hello Express!')
+//CORS Middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+    return res.status(200).json({})
+  }
+
+  next()
 })
 
-app.get('/api/users/:id', (_req, res) => {
+app.use(express.json())
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to My Vercel project!")
+})
+
+app.get("/api/users/:id", (_req, res) => {
   res.json({ id: _req.params.id })
 })
 
-app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
+app.get("/api/posts/:postId/comments/:commentId", (_req, res) => {
   res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
+})
+
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`)
 })
 
 export default app
