1ebb3ccdd3dd3b8d75a2db4d8d980d386acce1c9
Bright Mabhiza <specifixperts@gmail.com>
2025-11-15 16:22:17 +0200

    first 1.0.0.3

--- src/routes/catalog.ts
+++ src/routes/catalog.ts
@@ -1,11 +1,11 @@
 import express from "express"
 const router = express.Router()
 
-router.get("/api/users/:id", (_req, res) => {
+router.get("/:id", (_req, res) => {
   res.json({ id: _req.params.id })
 })
 
-router.get("/api/posts/:postId/comments/:commentId", (_req, res) => {
+router.get("/:postId/comments/:commentId", (_req, res) => {
   res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
 })
 
